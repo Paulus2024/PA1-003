0de083731077fabbf26c6bd53e279ef226a782c8
Someone4562 <christoferkevin560@gmail.com>
2025-04-21 14:42:07 +0700

    CRUD data pengurus desa
    jangan diubah

---
+++ app/Models/DataPengurusDesa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DataPengurusDesa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'data_pengurus_desa';
+    protected $primaryKey = 'id_data_pengurus_desa';
+
+
+    protected $fillable = [
+        'nama_data_pengurus_desa',
+        'jabatan_data_pengurus_desa',
+        'deskripsi_data_pengurus_desa',
+        'gambar_data_pengurus_desa'
+    ];
+
+    protected $nullable = [
+        'create_at',
+        'update_at'
+    ];
+}
