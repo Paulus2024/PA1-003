f7fa7a121ec22b8b0995d3015535fdbc95f6bf2f
Paulus2024 <reynsanpanjaitan20@gmail.com>
2025-05-06 17:27:36 +0700

    update_alatPertanian

--- database/migrations/2025_05_06_044533_rename_peminjaman_comlom_in_peminjaman_table.php
+++ database/migrations/2025_05_06_044533_rename_peminjaman_comlom_in_peminjaman_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::table('peminjaman', function (Blueprint $table) {
             //ganti nama kolom yang mengalami typo
+            // $table->renameColumn('pemijaman', 'peminjaman');
+            DB::statemant("ALTER TABLE `peminjaman` CHANGE `status_peminjaman` `status_peminjaman` ENUM('menunggu', 'disetujui', 'ditolak') NOT NULL DEFAULT `menunggu` ");
+            //         lakukan perubahan pada table peminjaman, ubah bagian status_peminjaman, pada status peminjaman ubah menjadi enum dengan pilihan menunggu, disetujui, ditolak dan harus diisi setidaknya dengan default menunggu
         });
     }
 
@@ -22,7 +26,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('peminjaman', function (Blueprint $table) {
-            //
+            //melakukan perubahan kembali jika di roollback suatu saat
+            DB::statement("ALTER TABLE `peminjaman` CHANGE `status_peminjaman` `status_peminjaman` ENUM('menunggu', 'disetujui', 'ditolak') NOT NULL DEFAULT `mmenunggu` ");
         });
     }
 };
